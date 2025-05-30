42b3c81c452ad60ccb781b38ddfd8253e16d206d
Yogi Saputro <yogitn19@gmail.com>
2025-05-31 06:21:21 +0700

    feat: update to php 8.2

--- apps/lakasir/lakasir-ubuntu-v1.sh
+++ apps/lakasir/lakasir-ubuntu-v1.sh
@@ -10,7 +10,9 @@ set -e
 SCRIPT_VERSION="v1.0.0"
 
 REPO_URL="https://github.com/lakasir/lakasir.git" # Please support the original author.
-APP_DIR="~/lakasir-pos"
+APP_DIR="$HOME/lakasir-pos"
+APP_VERSION="v1.1.5"
+PHP_VERSION="8.2"
 
 DB_USER=${1:-DEFAULT_USER}            # input for MySQL User with default value
 DB_PASS=${2:-DEFAULT_PASSWORD}        # input for MySQL password with default value
@@ -21,7 +23,7 @@ echo "=== Starting Lakasir POS Deployment Script ${SCRIPT_VERSION} ==="
 # 1. Repository installation step
 if [ ! -d "$APP_DIR" ]; then
   echo "Cloning repository..."
-  git clone "$REPO_URL" "$APP_DIR"
+  git clone --branch "$APP_VERSION" --depth 1 "$REPO_URL" "$APP_DIR"
 else
   echo "Repository already exists. Skipping clone..."
 fi
@@ -29,18 +31,26 @@ fi
 cd "$APP_DIR"
 
 # 2. PHP Installation Step
-if ! php -v | grep -q "8.1"; then
-  echo "Installing PHP 8.1..."
+if ! php -v | grep -q "8.2"; then
+  echo "Installing PHP 8.2..."
+  sudo apt install -y software-properties-common
   sudo add-apt-repository ppa:ondrej/php -y
   sudo apt update
-  sudo apt install -y php8.1 php8.1-cli php8.1-mbstring php8.1-xml php8.1-curl php8.1-mysql unzip
+
+  sudo apt install -y nginx php$PHP_VERSION php$PHP_VERSION-{fpm,cli,mbstring,bcmath,xml,zip,curl,common,mysql,intl}
+
+  sudo update-alternatives --set php /usr/bin/php8.2
+
+  sudo systemctl enable php$PHP_VERSION-fpm
+  sudo systemctl start php$PHP_VERSION-fpm
+
 else
-  echo "PHP 8.1 is already installed. Skipping PHP installation..."
+  echo "PHP 8.2 is already installed. Skipping PHP installation..."
 fi
 
 # 3. MySQL Installation Step
-if ! mysql --version | grep -qE "Distrib 5\.7|Distrib 8\."; then
-  echo "Installing MySQL 8..."
+if ! mysql --version; then
+  echo "Installing MySQL ..."
   sudo apt update
   sudo apt install -y mysql-server
   sudo systemctl enable mysql
@@ -115,6 +125,7 @@ sed -i "s/^DB_DATABASE=.*/DB_DATABASE=${DB_NAME}/" .env
 sed -i "s/^DB_USERNAME=.*/DB_USERNAME=${DB_USER}/" .env
 sed -i "s/^DB_PASSWORD=.*/DB_PASSWORD=${DB_PASS}/" .env
 sed -i "s/^APP_ENV=.*/APP_ENV=production/" .env
+sed -i "s/^APP_DEBUG=.*/APP_DEBUG=false/" .env
 
 php artisan key:generate
 php artisan migrate --path=database/migrations/tenant --seed
